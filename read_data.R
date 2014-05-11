8b20e5dd34a7f0c0741afbfcec801f272f792ad1
Peter Bronez <pbronez@deepmile.com>
2014-05-11 19:13:29 -0400

    Data import boilerplate

---
+++ read_data.R
@@ -0,0 +1,15 @@
+# Main.R
+
+# Load data from disk
+data <- read.table("household_power_consumption.txt", header = TRUE, sep=";",
+                    na.strings="?", colClasses=c("character", "character", "numeric",
+                    "numeric","numeric","numeric","numeric","numeric","numeric"))
+
+# Extract time information
+data$datetime <- paste(data$Date, data$Time)
+data$datetime <- strptime(data$datetime, format="%d/%m/%Y %T")
+
+# Isolate the time period of interest
+data <- subset(data, datetime >= as.POSIXlt("2007-02-01", format="%Y-%m-%d") & 
+                   datetime < as.POSIXlt("2007-02-03", format="%Y-%m-%d"))
+
