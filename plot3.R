8d0eedfd95f48c53c55774a753a88ff7e1a86e52
Peter Bronez <pbronez@deepmile.com>
2014-05-11 19:52:11 -0400

    Plot 3 complete.

--- plot3.R
+++ plot3.R
@@ -4,4 +4,11 @@
 source("read_data.R")
 
 # Energy Submetering Over Time
-# TODO
+png(file="plot3.png")
+plot(data$datetime, data$Sub_metering_1, type="line", col="black",
+     ylab="Energy sub metering", xlab="")
+lines(data$datetime, data$Sub_metering_2, type="line", col="red")
+lines(data$datetime, data$Sub_metering_3, type="line", col="blue")
+legend("topright", col=c("Black","Red","Blue"), lty = 1,
+       legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
